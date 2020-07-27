934fbeff99b8f2d4f393d473e4473e37683ac31b
manolachemiruna <manolacheandreea22@yahoo.com>
2020-07-27 05:00:59 -0700

    project setup and database connection

---
+++ Certifications/src/main/java/team7/Certifications/CertificationsApplication.java
@@ -0,0 +1,13 @@
+package team7.Certifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationsApplication.class, args);
+	}
+
+}
---
+++ Certifications/src/test/java/team7/Certifications/CertificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package team7.Certifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
